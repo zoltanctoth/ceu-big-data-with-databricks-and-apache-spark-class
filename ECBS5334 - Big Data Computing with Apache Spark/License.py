b3ef3308a867d5c2fa1f6fad8dc3aeceb36561c5
zoltanctoth <zoltanctoth>
2025-04-21 04:49:21 +0000

    Cosmetics

--- ECBS5334 - Big Data Computing with Apache Spark/License.py
+++ ECBS5334 - Big Data Computing with Apache Spark/License.py
@@ -1,8 +1,9 @@
 # Databricks notebook source
 # MAGIC %md # License Information
 # MAGIC
-# MAGIC  * A large proportion of this courseware is copied or built upon the original <a target='_blank' href='https://github.com/zoltanctoth/apache-spark-programming-with-databricks'>Apache Spark Programming  with Databricks course</a>, which uses the <a target='_blank' href='https://github.com/databricks-academy/apache-spark-programming-with-databricks/blob/published/LICENSE'>Creative Commons Zero v1.0 Universal
-# MAGIC </a> license. I made modifications to that courseware. 
+# MAGIC A large proportion of this courseware is copied or built upon the publicly available Apache Spark Programming with Databricks course under the <a target='_blank' href='https://github.com/databricks-academy/apache-spark-programming-with-databricks/blob/published/LICENSE'>Creative Commons Zero v1.0 Universal
+# MAGIC </a> license. 
 # MAGIC
+# MAGIC I made significant modifications to that courseware to accomodate the requirements of CEU's Big Data Computing Class, such as shifting the focus of it to open source Spark and improving it in some pedagogical aspects. 
 # MAGIC
-# MAGIC If you want to distribute this courseware, please distribute the original Databricks Courseware instead. This courseware has been tailored to CEU's Big Data Classes Curriculum. The original courseware covers a superset of what we cover here.
+# MAGIC <a href="https://www.linkedin.com/in/zoltanctoth/" target="_blank">Zoltan C. Toth</a>
